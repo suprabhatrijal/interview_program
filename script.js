4afffb0f68946436f431d778ebe2db814a7f0b37
Suprabhat Rijal <goodguysman@gmail.com>
2024-02-09 10:08:45 -0500

    Removed video feed

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-const video = document.getElementById('video')
+var video = document.createElement("video");
+video.setAttribute('id', 'video');
+video.setAttribute('width', '720');
+video.setAttribute('height', '560');
+video.setAttribute('autoplay', 'muted');
+document.body.appendChild(video);
+document.getElementById("video").style.display = "none";
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('https://storage.googleapis.com/model1-bucket/models'),
@@ -16,11 +22,14 @@ function startVideo() {
 }
 
 video.addEventListener('play', () => {
+  // document.getElementById("video").style.display = "inline"
   const canvas = faceapi.createCanvasFromMedia(video)
   document.body.append(canvas)
+  // document.body.remove("video")
   const displaySize = { width: video.width, height: video.height }
   faceapi.matchDimensions(canvas, displaySize)
   setInterval(async () => {
+    // console.log(video)
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
@@ -28,4 +37,5 @@ video.addEventListener('play', () => {
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
   }, 100)
+  // document.getElementById("video").style.display = "none"
 })
